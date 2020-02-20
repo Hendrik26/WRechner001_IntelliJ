da081595dbb83280d212a8909ee24df9925dad94
Hendrik26 <hendrik.herdler@gmail.com>
2020-02-20 21:19:57 +0100

    Program WRechner001_IntelliJ makes something.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,13 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 ///
 
@@ -11,7 +15,19 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Button button01 = new Button();
+        button01.setText("Write Hallo World");
+        button01.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Hallo World!");
+            }
+        });
+        // ---------------------------------------------------------
+        // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        // root.getChildrenUnmodifiable().add(button01);
+        StackPane root = new StackPane();
+        root.getChildren().add(button01);
+        // -----------------------------------------------------------------
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
